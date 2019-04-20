cd6775f4191171602672cd4199b6b58dfc684ce8
APTK008 <489567501@qq.com>
2019-04-20 17:18:53 +0800

    小于900

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int gitint = 100;
+            if (gitint<900)
+            {
+                MessageBox.Show("小于900", "提示框");
+            }
+            else
+            {
+                MessageBox.Show($"大于{gitint}", "提示框");
+            }
+           
+
+        }
+    }
+}
